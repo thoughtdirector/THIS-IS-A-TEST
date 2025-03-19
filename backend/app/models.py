a97f1536eb6ef3bc2cb219aefed50376a6cd3878
daniv <danivego2015@gmail.com>
2025-03-19 18:45:21 -0500

    Save terms_accepted as False by default (If not received from the client)

--- backend/app/models.py
+++ backend/app/models.py
@@ -65,7 +65,7 @@ class UserRegister(SQLModel):
     email: EmailStr = Field(max_length=255)
     password: str = Field(min_length=8, max_length=40)
     full_name: str | None = Field(default=None, max_length=255)
-    terms_accepted: bool = Field(default=True)
+    terms_accepted: bool = Field(default=False)
 
 
 # Properties to receive via API on update, all are optional
