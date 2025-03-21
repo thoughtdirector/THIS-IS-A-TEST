942b386fb7c15c9f3358ed2d174d068830afffe0
daniv <danivego2015@gmail.com>
2025-03-21 11:08:19 -0500

    Inlcude forms router in main app

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils, admin, clients
+from app.api.routes import forms, items, login, users, utils, admin, clients
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -9,3 +9,4 @@ api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(clients.router, prefix="/clients", tags=["clients"])
 api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
+api_router.include_router(forms.router, prefix="/forms", tags=["forms"])
